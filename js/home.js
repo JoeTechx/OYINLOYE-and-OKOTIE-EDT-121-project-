0de39ce4f3d6129c8f0654fcb2f2c08dec48ee17
JOEX <oyinjoe23@gmail.com>
2023-05-08 05:09:20 +0100

    feat: home stlying

--- js/home.js
+++ js/home.js
@@ -6,3 +6,11 @@ var typed = new Typed(".multiple-text", {
     loop: true
 })
 
+var typed = new Typed(".auto-text", {
+    strings: ["Frontend Developer", "copywriter"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
